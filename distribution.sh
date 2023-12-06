41975dc3496e073eb07c2ec58934e42c78dcb552
Ravi Bandaru <80599578+ravibandaru-lab@users.noreply.github.com>
2023-12-06 09:44:58 -0500

    Delete distribution.sh

--- distribution.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-python ./distribution.py \
-    --source_bed_file /jet/home/rbandaru/ocean_rbandaru/FRAGMA/data/HD_45_CG_FINAL_0.bed \
-    --mappability_file /jet/home/rbandaru/ocean_rbandaru/FRAGMA/data/HD_45.WGBS.frag.bed.gz \
-    
